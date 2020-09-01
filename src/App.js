8a2821829dc47381cbe798de1addac567971657d
Zeeshan Khan <zeeskhan1990@gmail.com>
2020-09-02 03:21:52 +0530

    Introducing change for list type change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import ListWrapper from "./ListWrapper";
 import {list} from "./constants";
 import request from "./request";
@@ -10,14 +10,13 @@ function App() {
   const [listType, setListType] = useState(list.type.top)
   const [storyList, setStoryList] = useState(null)
 
-  const loadListDetails = async (storyList, startIndex) => {
+  const loadListDetails = async (stories, startIndex) => {
     const endIndex = startIndex + (list.batchSize - 1) > list.maxSize - 1 ? list.maxSize - 1 : startIndex + (list.batchSize - 1)
     const allRequests = []
     for (let i=startIndex; i<=endIndex; i++) {
-      allRequests.push(request.get(`/item/${storyList[i]}.json`))
+      allRequests.push(request.get(`/item/${stories[i]}.json`))
     }
     const responses = await Promise.all(allRequests)
-    debugger
     setIsNextPageLoading(false)
     setHasNextPage(items.length < list.maxSize)
     setItems([...items].concat(
@@ -31,7 +30,6 @@ function App() {
     let storyData = null
     if(!storyList) {
       try {
-        debugger
         const response = await request.get(`/${listType}.json`)
         storyData = response.data
         setStoryList(storyData)
@@ -42,18 +40,18 @@ function App() {
     } else {
       loadListDetails(storyList, startIndex)      
     }
-    /* request.get(`/${listType}.json`).then(({data}) => {
-      console.log(data)
-      setHasNextPage(items.length < list.maxSize)
-      setIsNextPageLoading(false)
-      setItems([...items].concat(
-        new Array(list.batchSize).fill(true).map(() => ({ name: "Zeeshan Khan" }))
-      ))
-    }).catch(err => {
-      console.log("Error", err)
-      setIsNextPageLoading(false)
-    }) */
   };
+
+  const handleListTypeChange = () => {
+    setHasNextPage(true)
+    setIsNextPageLoading(false)
+    setItems([])
+    setStoryList(null)
+  }
+
+  useEffect(() => {
+    handleListTypeChange()
+  }, [listType])
   
   return (
       <Fragment>
